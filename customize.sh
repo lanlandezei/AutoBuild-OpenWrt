5a5e0580d270cc0fc808a1114c98a859e227232c
lanlandezei <71123196+lanlandezei@users.noreply.github.com>
2021-01-19 15:00:13 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,4 +6,7 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.31.190/g' openwrt/package/base-files/files/bin/config_generate
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
